242320918e2c2e82d409fb285caef25d28f84898
Jan Caha <jan.caha@outlook.com>
2019-12-22 00:45:10 +0100

    package info

---
+++ setup.py
@@ -0,0 +1,21 @@
+# pylint: skip-file
+from setuptools import setup, find_packages
+
+setup(name='gdalhelpers',
+      version='0.1.3',
+      description='GDAL helpers, checks and tools package',
+      url='https://github.com/JanCaha/gdalhelpers',
+      author='Jan Caha',
+      author_email='jan.caha@outlook.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'markdown',
+          'gdal',
+          'numpy',
+          'nose'
+      ],
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose']
+      )
