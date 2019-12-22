005d16fa92ada46ad59f88dc54ffb05fb88e137a
Jan Caha <jan.caha@outlook.com>
2019-12-22 16:58:16 +0100

    change get_geometry_list to return ogr.Geometry, change parameter names in functions

--- gdalhelpers/helpers/layer_helpers.py
+++ gdalhelpers/helpers/layer_helpers.py
@@ -2,16 +2,16 @@ from osgeo import ogr, osr
 from typing import List
 
 
-def create_layer_points(los_ds: ogr.DataSource, srs: osr.SpatialReference, layer_name: str):
-    return los_ds.CreateLayer(layer_name, srs, ogr.wkbPoint, ['OVERWRITE=YES'])
+def create_layer_points(ds: ogr.DataSource, srs: osr.SpatialReference, layer_name: str):
+    return ds.CreateLayer(layer_name, srs, ogr.wkbPoint, ['OVERWRITE=YES'])
 
 
-def create_layer_points_25d(los_ds: ogr.DataSource, srs: osr.SpatialReference, layer_name: str):
-    return los_ds.CreateLayer(layer_name, srs, ogr.wkbPoint25D, ['OVERWRITE=YES'])
+def create_layer_points_25d(ds: ogr.DataSource, srs: osr.SpatialReference, layer_name: str):
+    return ds.CreateLayer(layer_name, srs, ogr.wkbPoint25D, ['OVERWRITE=YES'])
 
 
-def create_layer_lines_25d(los_ds: ogr.DataSource, srs: osr.SpatialReference, layer_name: str):
-    return los_ds.CreateLayer(layer_name, srs, ogr.wkbLineString25D, ['OVERWRITE=YES'])
+def create_layer_lines_25d(ds: ogr.DataSource, srs: osr.SpatialReference, layer_name: str):
+    return ds.CreateLayer(layer_name, srs, ogr.wkbLineString25D, ['OVERWRITE=YES'])
 
 
 def add_fields_from_dict(layer: ogr.Layer, fields_types: dict):
@@ -27,13 +27,13 @@ def add_values_from_dict(feature: ogr.Feature, fields_values: dict):
         feature.SetField(field, value)
 
 
-def get_geometry_list(layer: ogr.Layer) -> List[str]:
+def get_geometry_list(layer: ogr.Layer) -> List[ogr.Geometry]:
 
     feature_list: list = [None] * layer.GetFeatureCount()
     i: int = 0
 
     for feature in layer:
-        feature_list[i] = feature.GetGeometryRef().ExportToWkt()
+        feature_list[i] = feature.GetGeometryRef().Clone()
         i += 1
 
     return feature_list
