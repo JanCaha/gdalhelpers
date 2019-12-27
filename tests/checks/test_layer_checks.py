d1178d6b0abbaeb51b14a8d9cea2aad1825bedf4
Jan Caha <jan.caha@outlook.com>
2019-12-27 11:47:28 +0100

    tests for srs layers checks with new dataset

--- tests/checks/test_layer_checks.py
+++ tests/checks/test_layer_checks.py
@@ -1,11 +1,13 @@
-from osgeo import ogr
+from osgeo import ogr, gdal, osr
 import unittest
 import tempfile
 import os
 import gdalhelpers.checks.layer_checks as layer_checks
 
 POINTS_PATH = os.path.join(os.path.dirname(__file__), "..", "test_data", "points.gpkg")
+POINTS_4326_PATH = os.path.join(os.path.dirname(__file__), "..", "test_data", "points_4326.gpkg")
 POINT_PATH = os.path.join(os.path.dirname(__file__), "..", "test_data", "single_point.gpkg")
+
 
 class LayerChecksTests(unittest.TestCase):
 
@@ -23,8 +25,18 @@ class LayerChecksTests(unittest.TestCase):
         field_def = ogr.FieldDefn("field_string", ogr.OFTString)
         self.layer_point.CreateField(field_def)
 
+        #load point layer
+        self.ds_loaded = ogr.Open(POINTS_PATH)
+        self.layer_loaded = self.ds_loaded.GetLayer()
+
+        # load point layer with srs 4326
+        self.ds_loaded_4326 = ogr.Open(POINTS_4326_PATH)
+        self.layer_loaded_4326 = self.ds_loaded_4326.GetLayer()
+
     def tearDown(self):
         self.ds_points = None
+        self.layer_loaded = None
+        self.ds_temp = None
 
     def test_does_field_exist(self):
 
@@ -71,3 +83,15 @@ class LayerChecksTests(unittest.TestCase):
 
         with self.assertRaisesRegex(AttributeError, "must contain only"):
             layer_checks.check_number_of_features(ds_points.GetLayer(), "points", number=1)
+
+    def test_check_is_projected(self):
+        self.assertIsNone(layer_checks.check_is_projected(self.layer_loaded, "layer_loaded"))
+
+        with self.assertRaisesRegex(ValueError, "layer does not have Spatial Reference specified"):
+            layer_checks.check_is_projected(self.layer_point, "layer_points")
+
+    def test_check_layers_sr_are_same(self):
+        self.assertIsNone(layer_checks.check_layers_sr_are_same(self.layer_loaded, self.layer_loaded))
+
+        with self.assertRaisesRegex(ValueError, "Spatial Reference of both layers"):
+            layer_checks.check_layers_sr_are_same(self.layer_loaded_4326, self.layer_loaded)
