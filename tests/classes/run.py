782a20868c48efe6e0dbe543c408b8e49b2a51be
Jan Caha <jan.caha@outlook.com>
2019-12-27 17:06:03 +0100

    remove testing file

--- tests/classes/run.py
+++
@@ -1,9 +0,0 @@
-import os
-
-print(os.path.dirname(__file__))
-
-print(os.path.join(os.path.realpath(".."), "test_data", "points.gpkg"))
-
-POINTS_PATH = os.path.join(os.path.dirname(__file__), "..", "tests", "points.gpkg")
-
-print(POINTS_PATH)
