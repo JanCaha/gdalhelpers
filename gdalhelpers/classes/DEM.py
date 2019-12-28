0dbb470e76b730f09103872a27277930af0c8348
Jan Caha <jan.caha@outlook.com>
2019-12-28 22:54:30 +0100

    class documentation and minor fixes

--- gdalhelpers/classes/DEM.py
+++ gdalhelpers/classes/DEM.py
@@ -11,11 +11,48 @@ from gdalhelpers.checks import geometry_checks
 
 
 class DEM:
+    """
+    Class representing digital elevation model. Based on `gdal.Dataset` with additional functions.
+
+    Attributes
+    ----------
+    path : str
+        Path to the stored dataset.
+
+    ds : gdal.Dataset
+        Raster dataset in `gdal`.
+
+    projection : osr.SpatialReference
+        `osr.SpatialReference` for from `ds`.
+
+    gt
+        Result of `ds.GetGeoTransform()` function.
+
+    nx, ny : int
+        Raster size in x and y direction in cells.
+
+    no_data : float
+        Value from functions `ds.GetRasterBand(1).GetNoDataValue()`.
+
+    np_array : np.array
+        Two dimensional numpy array of values from `ds`.
+    """
 
     __band_number = 1
     __gdal_nodata_default = -3.4028e+38
 
     def __init__(self, path: str, epsg: int = None):
+        """
+        Construct function.
+
+        Parameters
+        ----------
+        path : str
+            Path to load the data from.
+
+        epsg : int, optional
+            EPSG code of raster projection.
+        """
 
         if not os.path.isfile(path):
             raise IOError("File at specified path `{0}` does not exist.".format(path))
@@ -64,6 +101,18 @@ class DEM:
 
     @classmethod
     def from_gdal_raster(cls, gdal_raster: gdal.Dataset):
+        """
+        Class method to build `DEM` from `gdal.Dataset`.
+
+        Parameters
+        ----------
+        gdal_raster : gdal.Dataset
+            Build `DEM` based on allready loaded raster data.
+
+        Returns
+        -------
+        None
+        """
 
         if not isinstance(gdal_raster, gdal.Dataset):
             raise TypeError("`gdal_raster` has to be of type `gdal.Dataset`. "
@@ -72,38 +121,109 @@ class DEM:
         return cls(path=gdal_raster.GetDescription())
 
     def __str__(self):
+        """
+        String representation of class `DEM`.
+
+        Returns
+        -------
+        str
+            Represetation of DEM.
+        """
+
         return "Raster from `{0}` with sizes(`{1}`, `{2}`) using projection `{3}`.".format(self.path,
                                                                                            self.nx,
                                                                                            self.ny,
                                                                                            self.ds.GetProjection())
 
     def __del__(self):
+        """
+        Upon deletion of the object close connection and clear array.
+
+        Returns
+        -------
+        None
+        """
         self.ds = None
         self.destroy_array()
 
     def get_min_pixel_size(self) -> float:
+        """
+        Get minimal pixel size in either row or column direction.
+        
+        Returns
+        -------
+        float
+            Minimal pixel size in either row or column direction.
+        """
         return abs(min(self.gt[1], self.gt[5]))
 
     def __check_value_is_nodata(self, value: numbers.Number) -> bool:
         return (value < self.__gdal_nodata_default) or math_helpers.is_almost_equal(value, self.no_data)
 
     def load_array(self) -> None:
+        """
+        Read raster values into numpy array.
+
+        Returns
+        -------
+        None
+        """
         self.np_array = self.ds.GetRasterBand(self.__band_number).ReadAsArray()
 
     def destroy_array(self) -> None:
+        """
+        Clear numpy array with raster values.
+
+        Returns
+        -------
+        None
+        """
         self.np_array = None
 
     def get_nodata_value(self) -> float:
+        """
+        Get no data value of the raster.
+
+        Returns
+        -------
+        float
+            No data value of the raster.
+        """
         return self.no_data
 
     def get_max_size(self) -> float:
+        """
+        Get larger size of the raster in map units. Calculated as pixel width/height times raster width/height.
+
+        Returns
+        -------
+        float
+            Maximal raster size.
+        """
         return max(self.gt[1] * self.nx, self.gt[5] * self.ny)
 
     def get_diagonal_size(self) -> float:
+        """
+        Get diagonal size of the raster in map units.
+
+        Returns
+        -------
+        float
+            Raster size in diagonal direction.
+        """
         size = math.sqrt(math.pow(self.gt[1] * self.nx, 2) + math.pow(self.gt[5] * self.ny, 2))
         return size
 
     def get_bounding_box(self) -> ogr.Geometry:
+        """
+        Get bounding box of the raster.
+
+        Returns
+        -------
+        ogr.Geometry
+            Bounding box of the raster.
+        """
+
         x1 = self.gt[0]
         x2 = self.gt[0] + self.nx * self.gt[1]
         y1 = self.gt[3]
@@ -123,7 +243,19 @@ class DEM:
 
     # code from http://gis.stackexchange.com/questions/7611/bilinear-interpolation-of-point-data-on-a-raster-in-python
     def get_value_bilinear(self, px: float, py: float) -> float:
-        """Bilinear interpolated point at (px, py) on raster"""
+        """
+        Get bilinear interpolated value from the raster at point (px, py).
+
+        Parameters
+        ----------
+        px, py : float
+            Point coordinates in raster units.
+
+        Returns
+        -------
+        float
+            Interpolated value of the raster at specified location.
+        """
 
         if not isinstance(px, numbers.Number):
             raise TypeError("px must be number. The variable is of `{}`.".format(type(px)))
@@ -177,7 +309,30 @@ class DEM:
                 (self.np_array[iy2, ix1] * dx2 * dy1) / div +
                 (self.np_array[iy2, ix2] * dx1 * dy1) / div)
 
-    def get_values_bilinear(self, positions: List[List[float]]):
+    def get_values_bilinear(self, positions: List[List[float]]) -> List[float]:
+        """
+        The same function as `get_value_bilinear()` on vectorized for list definition of locations.
+
+        Parameters
+        ----------
+        positions : list of list of float
+            Position for interpolation of raster values. The format should be
+            `positions=[[px1,py1],[px2,py2],[px3,py3]]`.
+
+        Returns
+        -------
+        list of float
+            For every element of list of `positions` one element in list.
+
+        Raises
+        ------
+        ValueError
+            If element of positions is not list with `len() == 2`.
+
+        See Also
+        --------
+        get_value_bilinear : Get value bilinear
+        """
 
         values: list = [None] * len(positions)
         i: int = 0
@@ -195,8 +350,32 @@ class DEM:
 
         return values
 
-    def get_values_points_bilinear(self, points: List[ogr.Geometry]):
+    def get_values_points_bilinear(self, points: List[ogr.Geometry]) -> List[float]:
+        """
+        The same function as `get_values_bilinear()` but with different parameter.
 
+        Parameters
+        ----------
+        points : list of ogr.Geometry
+            List of `ogr.Geometry` points (`ogr.wkbPoint, ogr.wkbPoint25D, gr.wkbPointM, ogr.wkbPointZM`) to get raster
+            value at.
+
+        Returns
+        -------
+        list of float
+            For every element of list of `points` one element in list.
+
+        Raises
+        ------
+        ValueError
+            If element of `points` is not `ogr.Geometry` with type `ogr.wkbPoint, ogr.wkbPoint25D, gr.wkbPointM or
+             ogr.wkbPointZM`.
+
+        See Also
+        --------
+        get_value_bilinear : Get value bilinear
+        get_values_bilinear : Get values bilinear
+        """
         values: list = [None] * len(points)
         i: int = 0
 
